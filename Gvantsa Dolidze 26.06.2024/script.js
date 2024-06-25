c8f383ed4879176fc834171d3552cfc2b959f5f1
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-06-25 23:50:33 +0400

    create homework with html and css content

---
+++ Gvantsa Dolidze 26.06.2024/script.js
@@ -0,0 +1,17 @@
+//   API:
+//   https://api.restful-api.dev/objects
+
+
+//   Request JSON Example:
+//   {
+//   "name": "Apple MacBook Pro 16",
+//   "data": {
+//   "year": 2019,
+//   "price": 1849.99,
+//   "CPU model": "Intel Core i9",
+//   "Hard disk size": "1 TB"
+//   }
+//   
+
+// გააკეთეთ ფორმა და POST მეთოდით გააგზავნეთ ფორმაში შეყვანილი ინფორმაცია ზემოთ აღნიშნულ მისამართზე,
+// რესპონსში დაბრუნებული დატა შეინახეთ მასივში და გამოიტანეთ ლისტის სახით ფორმის გვერძე.
