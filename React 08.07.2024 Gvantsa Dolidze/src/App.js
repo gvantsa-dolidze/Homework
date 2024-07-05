50e4b00454e61f094462acfb8a81b116c9ac40a7
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-07-05 23:40:26 +0400

    create new homework

---
+++ React 08.07.2024 Gvantsa Dolidze/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from "./components/main/Button"
+function App() {
+  return (
+    <div className="App">
+     <Button />
+    </div>
+  );
+}
+
+export default App;
