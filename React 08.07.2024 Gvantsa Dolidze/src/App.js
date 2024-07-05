4dd4a8f65af6fd68925e6b82f2211a0a102a95aa
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-07-06 00:05:47 +0400

    create else if

--- React 08.07.2024 Gvantsa Dolidze/src/App.js
+++ React 08.07.2024 Gvantsa Dolidze/src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import Button from "./components/main/Button"
 function App() {
   return (
     <div className="App">
-     <Button />
+     <Button theme={"outline"}/>
     </div>
   );
 }
--- React 08.07.2024 Gvantsa Dolidze/src/components/main/Button.jsx
+++ React 08.07.2024 Gvantsa Dolidze/src/components/main/Button.jsx
@@ -1,10 +1,35 @@
-import React from "react";
-const Button = () => {
+const Button = (params) => {
+    
+if(params.theme === "primary"){
+    return (
+        <div>
+            <button className="btn btn-secondary">Click</button>
+        </div>
+    )
+} else if(params.theme === "secondary") {
+    return (
+        <div>
+            <button className="btn btn-outline">Click</button>
+        </div>
+    )
+} else {
     return (
         <div>
             <button className="btn btn-primary">Click</button>
         </div>
     )
 }
+}
+
+
+
+
+
+    // return (
+    //     <div>
+    //         <button className="btn btn-primary">Click</button>
+    //     </div>
+    // )
+
 
 export default Button; 
