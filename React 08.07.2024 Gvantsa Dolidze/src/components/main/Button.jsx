b9b1db4808eb618a1dd4180fedffafe58bd72539
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-07-06 01:42:38 +0400

    button works

--- React 08.07.2024 Gvantsa Dolidze/src/components/main/Button.jsx
+++ React 08.07.2024 Gvantsa Dolidze/src/components/main/Button.jsx
@@ -1,35 +1,7 @@
-const Button = (params) => {
-    
-if(params.theme === "primary"){
+const Button = ({theme}) => {
     return (
-        <div>
-            <button className="btn btn-secondary">Click</button>
-        </div>
+        <button className={theme}>Click</button>
     )
-} else if(params.theme === "secondary") {
-    return (
-        <div>
-            <button className="btn btn-outline">Click</button>
-        </div>
-    )
-} else {
-    return (
-        <div>
-            <button className="btn btn-primary">Click</button>
-        </div>
-    )
-}
-}
+};
 
-
-
-
-
-    // return (
-    //     <div>
-    //         <button className="btn btn-primary">Click</button>
-    //     </div>
-    // )
-
-
-export default Button; 
+export default Button;
