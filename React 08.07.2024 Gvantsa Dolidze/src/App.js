5252aa9beecbcea0a702e47f1918de32b9bf5704
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-07-06 16:26:10 +0400

    add ka/en

--- React 08.07.2024 Gvantsa Dolidze/src/App.js
+++ React 08.07.2024 Gvantsa Dolidze/src/App.js
@@ -5,17 +5,22 @@ import Header from "./components/header/Header";
 const App = () => {
   return (
     <div className="App">
-         <Header logo="LoGo"
-                 nav1="Main"
-                 nav2="Contact"
-                 nav3="About us"
-                 nav4="Products"
-         />
-       
-       <h1>Click the "Change" button</h1>
-      <CheckButton text="Change"/>
+      <Header version="ka"
+        logo="LoGo"
+        nav_en_1="Main"
+        nav_en_2="Contact"
+        nav_en_3="About us"
+        nav_en_4="Products"
+        nav_ka_1="მთავარი"
+        nav_ka_2="კონტაქტი"
+        nav_ka_3="ჩვენს შესახებ"
+        nav_ka_4="პროდუქტები"
+      />
+
+      <h1>Click the "Change" button</h1>
+      <CheckButton text="Change" />
     </div>
   );
-}
+};
 
 export default App;
--- React 08.07.2024 Gvantsa Dolidze/src/components/header/Header.jsx
+++ React 08.07.2024 Gvantsa Dolidze/src/components/header/Header.jsx
@@ -1,19 +1,34 @@
-import './Header.css'
+import "./Header.css";
+const Header = ({ version, logo, nav_en_1, nav_en_2, nav_en_3, nav_en_4, nav_ka_1, nav_ka_2, nav_ka_3, nav_ka_4 }) => {
 
-const Header = ({logo, nav1, nav2, nav3, nav4}) => {
-
-
-    return(
+    if( version === "en"){
+  return (
+    <header>
+      <h2>{logo}</h2>
+      <nav className="header_nav">
+        <a href="/link">{nav_en_1}</a>
+        <a href="/link">{nav_en_2}</a>
+        <a href="/link">{nav_en_3}</a>
+        <a href="/link">{nav_en_4}</a>
+      </nav>
+    </header>
+  );
+} else if(version === "ka") {
+    return (
         <header>
-            <h2>{logo}</h2>
-            <nav className="header_nav">
-                <a href="/link">{nav1}</a>
-                <a href="/link">{nav2}</a>
-                <a href="/link">{nav3}</a>
-                <a href="/link">{nav4}</a>
-            </nav>
-        </header>
+      <h2>{logo}</h2>
+      <nav className="header_nav">
+        <a href="/link">{nav_ka_1}</a>
+        <a href="/link">{nav_ka_2}</a>
+        <a href="/link">{nav_ka_3}</a>
+        <a href="/link">{nav_ka_4}</a>
+      </nav>
+    </header>
+    )
+} else {
+    return(
+        <h3 className="text-danger">Header not found 404</h3>
     )
 }
-
+};
 export default Header;
